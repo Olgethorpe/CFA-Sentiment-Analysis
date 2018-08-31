812f6d8dbd18a221204b73371c02729c478db8fc
Dominik Ondusko <d.ondusko@hotmail.com>
2018-08-31 01:39:31 -0700

    Added Probability Calibration model analysis and added support for multithreading between classifier analysis

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CFA-Sentiment-Analysis
 An analytical approach to decide sentiment on Twitter tweets
+
+# Model Analysis
+Each model has underwent a test of accuracy to show which configuration will be suited best for model selection which can be viewed in the "Model Results Folder"
--- generateModels.py
+++ generateModels.py
@@ -4,14 +4,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.calibration import CalibratedClassifierCV
 from numpy import arange, split
-from collections import OrderedDict
+from collections import OrderedDict 
 from matplotlib import pyplot
+from concurrent.futures import ThreadPoolExecutor
 
 def main():
     models = OrderedDict([('Logisitic Regression', LogisticRegression()), ('Random Forest', RandomForestClassifier(n_jobs = -1)), 
                           ('Gaussian Naive Bayes', GaussianNB()), ('Multinomial Naive Bayes', MultinomialNB()),
-                          ('Bernoulli Naive Bayes', BernoulliNB())])
+                          ('Bernoulli Naive Bayes', BernoulliNB()), ('Probability Calibration', CalibratedClassifierCV())])
     modelAccuracies = OrderedDict()
     modelXvals = None
     dataFile = Path('Data/Tweets.csv')
@@ -20,29 +22,10 @@ def main():
     X = vectorizer.fit_transform(data.text.values).toarray()
     Y = data.airline_sentiment.values
     dataPcts = [(x * 100 * .1) / 100 for x in range(1, 10)]
-    figCount = 0
-    for key, model in models.items():
-        accuracies = OrderedDict()
-        for trainPct, testPct in zip(dataPcts, dataPcts[::-1]):
-            trainVal = int(X.shape[0] * trainPct)
-            testVal = int(X.shape[0] * testPct)
-            accKey = '{}/{}'.format(int(trainPct * 100), int(testPct * 100))
-            Xtrain = X[0:trainVal]
-            Ytrain = Y[0:trainVal]
-            Xtest = X[trainVal:]
-            Ytest = Y[trainVal:]
-            accuracies[accKey] = model.fit(Xtrain, Ytrain).score(Xtest, Ytest)
-        modelAccuracies[key] = accuracies
-        pyplot.figure(figCount)
-        pyplot.plot(accuracies.keys(), accuracies.values())
-        pyplot.ylabel('Accuracy')
-        pyplot.xlabel('Train/Test Data Percentage Split')
-        pyplot.title('{} Accuracy'.format(key))
-        pyplot.savefig('Model Results/{} Accuracy.png'.format(key))
-        figCount += 1
-        print('Finished {} analysis'.format(key))
-    figCount += 1
-    pyplot.figure(figCount)
+    with ThreadPoolExecutor(max_workers = len(models)) as executor:
+        for key, model, count in zip(models.keys(), models.values(), range(len(models))):
+            executor.submit(compute, X, Y, model, dataPcts, modelAccuracies, key, count)
+    pyplot.figure(len(models) + 1)
     pyplot.ylabel('Accuracy')
     pyplot.xlabel('Train/Test Data Percentage Split')
     pyplot.title('Comparative Accuracy of All Models')
@@ -50,6 +33,27 @@ def main():
         pyplot.plot(accuracy.keys(), accuracy.values(), label = modelName)
     pyplot.legend()
     pyplot.savefig('Model Results/Comparative Accuracy.png')
+
+def compute(X, Y, model, dataPcts, modelAccuracies, key, figCount):
+    accuracies = OrderedDict()
+    for trainPct, testPct in zip(dataPcts, dataPcts[::-1]):
+        trainVal = int(X.shape[0] * trainPct)
+        testVal = int(X.shape[0] * testPct)
+        accKey = '{}/{}'.format(int(trainPct * 100), int(testPct * 100))
+        Xtrain = X[0:trainVal]
+        Ytrain = Y[0:trainVal]
+        Xtest = X[trainVal:]
+        Ytest = Y[trainVal:]
+        accuracies[accKey] = model.fit(Xtrain, Ytrain).score(Xtest, Ytest)
+    modelAccuracies[key] = accuracies
+    pyplot.figure(figCount)
+    pyplot.plot(accuracies.keys(), accuracies.values())
+    pyplot.ylabel('Accuracy')
+    pyplot.xlabel('Train/Test Data Percentage Split')
+    pyplot.title('{} Accuracy'.format(key))
+    pyplot.savefig('Model Results/{} Accuracy.png'.format(key))
+    figCount += 1
+    print('Finished {} analysis'.format(key))
 
 if __name__ == '__main__':
     main()
