0f39adab22e854f567c65f10978aa52c6c7740cb
Dominik Ondusko <d.ondusko@hotmail.com>
2018-09-24 23:29:18 -0400

    Deleted wrong files

--- Vectorizers.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
-# Stores the different vectorizers
-class Vectorizers(Enum):
-    Tf_idf = TfidfVectorizer()
-    Unigram = CountVectorizer()
-    Bigram = CountVectorizer(ngram_range = (2,2))
