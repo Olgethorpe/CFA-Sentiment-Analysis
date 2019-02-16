be784dba9771f117bec34c9c38d215101181f10a
Olgethorpe <dondusko@fordham.edu>
2019-02-16 13:46:13 -0500

    Added models

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import pathlib
 import pandas as pd
 import itertools
 import os
+import joblib
 
 class Vectorizers(enum.Enum):
     """Store the vectorizers"""
@@ -144,10 +145,13 @@ class CFAUtils:
 
 def main():
     """Main definition of the program"""
+
     util = CFAUtils()
     data_combs = util.powerset(Vectorizers)
     benchmark_folder = pathlib.Path('data/benchmark_data').resolve()
+    gen_model_folder = pathlib.Path('data/general_models').resolve()
     os.makedirs(benchmark_folder, exist_ok=True)
+    os.makedirs(gen_model_folder, exist_ok=True)
     raw_data_file = pathlib.Path('data/raw/tweets.csv').resolve()
     raw_data = pd.read_csv(
         raw_data_file,
@@ -163,6 +167,22 @@ def main():
         test_size=.2,
         random_state=42
     )
+    index_combs = []
+    for comb in data_combs:
+        folder_name = ''
+        for v in comb:
+            folder_name += v.name
+            if len(comb) > 1 and v != comb[-1]:
+                folder_name += '+'
+        index_combs.append(folder_name)
+    model_results = pd.DataFrame(
+        index=pd.MultiIndex.from_product(
+            [index_combs, [x.name for x in CVModels]]
+        ),
+        columns=pd.MultiIndex.from_product(
+            [[0, -1, 1], ['Precision', 'Recall', 'F1', 'Accuracy']]
+        )
+    )
     for comb in data_combs:
         folder_name = ''
         for vectorizer in comb:
@@ -171,15 +191,50 @@ def main():
                 folder_name += '+'
         union = FeatureUnion([(v.name, v.value) for v in comb])
         X_fitted_data = union.fit_transform(X_train)
-        gen_model_data_folder = benchmark_folder.joinpath(folder_name)
-        os.makedirs(gen_model_data_folder, exist_ok=True)
+        X_test_data = union.fit_transform(X_test)
+        cv_data_folder = benchmark_folder.joinpath(folder_name)
+        model_folder = gen_model_folder.joinpath(folder_name)
+        os.makedirs(cv_data_folder, exist_ok=True)
+        os.makedirs(model_folder, exist_ok=True)
         for CV_m in CVModels:
-            model_file = '{}.csv'.format(CV_m.name)
-            gen_model_file = gen_model_data_folder.joinpath(model_file)
-            if not gen_model_file.exists():
+            result_file = '{}.csv'.format(CV_m.name)
+            model_file = '{}.joblib'.format(CV_m.name)
+            cv_results_file = cv_data_folder.joinpath(result_file)
+            gen_model_file = model_folder.joinpath(model_file)
+            if not cv_results_file.exists():
                 CV_m.value.fit(X_fitted_data, Y_train)
                 results = pd.DataFrame.from_dict(CV_m.value.cv_results_)
-                results.to_csv(gen_model_file)
+                results.to_csv(cv_results_file)
+            elif not gen_model_file.exists():
+                results = pd.read_csv(cv_results_file)
+                results.sort_values('mean_test_F1')
+                params = json.loads(
+                    results.iloc[0]['params'].replace("'", "\""
+                    ).replace("classification__", '')
+                )
+                if CV_m.name == 'M1':
+                    m = LogisticRegression(**params, n_jobs=-1, random_state=42)
+                elif CV_m.name == 'M2':
+                    m = MultinomialNB(**params)
+                elif CV_m.name == 'M3':
+                    m = RandomForestClassifier(
+                        **params,
+                        n_jobs=-1,
+                        random_state=42
+                    )
+                elif CV_m.name == 'M4':
+                    m = SVC(**params, random_state=42)
+                elif CV_m.name == 'M5':
+                    m = Perceptron(**params, max_iter=1000, tol=1e-3)
+                if gen_model_file.exists():
+                    m.fit(X_fitted_data, Y_train)
+                    joblib.dump(m, gen_model_file)
+            else:
+                m = joblib.load(gen_model_file)
+                print(X_test_data.shape)
+                y_pred = m.predict(X_test_data)
+                print(y_pred)
+                print(f1_score())
 
 if __name__ == '__main__':
     main()
